e6230cc2981a644685997713fcf7478e68534b0c
Đorđe Atanackov <djordje_atanackov@uns.ac.rs>
2022-01-21 02:36:48 +0100

    Initial commit repair

---
+++ ParcelaService/Entities/DataContext/ParcelaContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParcelaService.Entities.DataContext
+{
+    public class ParcelaContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public ParcelaContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<Klasa> Klasa { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("ParcelaDB"));
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Klasa>()
+                .HasData(new
+                {
+                    ID_Klasa = Guid.Parse("f39c9623-c6b6-48cb-b4dd-0340c7431870"),
+                    KlasaNaziv = "Prva"
+                },
+                new
+                {
+                    ID_Klasa = Guid.Parse("0ec59e12-b271-471f-9a13-5c9c8ed0eda7"),
+                    KlasaNaziv = "Druga"
+                },
+                new
+                {
+                    ID_Klasa = Guid.Parse("e496b563-abb9-48a9-8972-800f41a4a3a1"),
+                    KlasaNaziv = "Treca"
+                }
+                );
+        }
+    }
+}
--- ParcelaService/Startup.cs
+++ ParcelaService/Startup.cs
@@ -1,9 +1,13 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using ParcelaService.Entities.DataContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,7 +27,18 @@ namespace ParcelaService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+
+            services.AddControllers();
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ParcelaService", Version = "v1" });
+            });
+
+            services.AddDbContext<ParcelaContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,16 +47,11 @@ namespace ParcelaService
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ParcelaService v1"));
             }
 
             app.UseHttpsRedirection();
-            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -49,7 +59,7 @@ namespace ParcelaService
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapRazorPages();
+                endpoints.MapControllers();
             });
         }
     }
