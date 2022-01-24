43dd0c3a87fc9555ce9bfb030e1171dfe3fc9481
Vuk Pekez <vukpekez@uns.ac.rs>
2022-01-24 10:55:45 +0100

    Add Entities and DbContext

---
+++ DocumentService/DbContext/DokumentDbContext.cs
@@ -0,0 +1,24 @@
+﻿using DocumentService.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DocumentService.DbContext
+{
+    public class DokumentDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbSet<Dokument> Dokumenti { get; set; }
+        public DbSet<TipDokumenta> TipoviDokumenta { get; set; }
+
+        public DokumentDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("DokumentDB"));
+        }
+    }
+}
---
+++ DocumentService/Entities/Dokument.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DocumentService.Entities
+{
+    [Table("Dokument")]
+    public class Dokument
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        
+        public string ZavodniBroj { get; set; }
+
+        public DateTime Datum { get; set; } = DateTime.Now;
+
+        public DateTime DatumDonosenjaDokumenta { get; set; } = DateTime.Now;
+        
+        public Guid TipDokumentaId { get; set; }
+        public TipDokumenta TipDokumenta { get; set; }
+    }
+}
