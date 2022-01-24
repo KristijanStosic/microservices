8fb1749394d3012b8092bfffa279f13f6f99e679
GavriloS <gavrilostanic2@gmail.com>
2022-01-24 17:28:44 +0100

    Add db context

---
+++ KupacService/KupacService/Entities/DataContext/KupacContext.cs
@@ -0,0 +1,27 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KupacService.Entities.DataContext
+{
+    public class KupacContext : DbContext
+    {
+
+        private readonly IConfiguration _configuration;
+        public KupacContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            this._configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+           optionsBuilder.UseSqlServer(_configuration.GetConnectionString("KupacDb"));
+        }
+
+
+    }
+}
--- KupacService/KupacService/Startup.cs
+++ KupacService/KupacService/Startup.cs
@@ -32,6 +32,8 @@ namespace KupacService
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "KupacService", Version = "v1" });
             });
+
+            services.AddDbContext<AdresaContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +56,9 @@ namespace KupacService
             {
                 endpoints.MapControllers();
             });
+
+
+
         }
     }
 }
