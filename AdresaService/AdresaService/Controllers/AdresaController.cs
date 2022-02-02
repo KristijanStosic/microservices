93ed407ed80a1ecc5580af9df66f9b347b48d815
GavriloS <gavrilostanic2@gmail.com>
2022-02-02 13:30:24 +0100

    Add logging to AdresaController

--- AdresaService/AdresaService/Controllers/AdresaController.cs
+++ AdresaService/AdresaService/Controllers/AdresaController.cs
@@ -1,10 +1,13 @@
 ﻿using AdresaService.Data.Interfaces;
 using AdresaService.Entities;
 using AdresaService.Model.Adresa;
+using AdresaService.ServiceCalls;
 using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,12 +22,14 @@ namespace AdresaService.Controllers
         private readonly IAdresaRepository _adresaRepository;
         private readonly IMapper _mapper;
         private readonly LinkGenerator _linkGenerator;
+        private readonly ILoggerService _loggerService;
 
-        public AdresaController(IAdresaRepository adresaRepository, IMapper mapper, LinkGenerator linkGenerator)
+        public AdresaController(IAdresaRepository adresaRepository, IMapper mapper, LinkGenerator linkGenerator, ILoggerService loggerService)
         {
             this._adresaRepository = adresaRepository;
             this._mapper = mapper;
             this._linkGenerator = linkGenerator;
+            this._loggerService = loggerService;
         }
 
         [HttpGet]
@@ -34,9 +39,10 @@ namespace AdresaService.Controllers
 
             if (adrese == null || adrese.Count == 0)
             {
+                await _loggerService.Log(LogLevel.Warning, "GetAdrese", "Lista adresa je prazna ili null");
                 return NoContent();
             }
-
+            await _loggerService.Log(LogLevel.Information, "GetAdrese", "Lista adresa je uspešno pronađena");
             return Ok(_mapper.Map<List<AdresaDto>>(adrese));
         }
 
@@ -47,8 +53,10 @@ namespace AdresaService.Controllers
 
             if (adresa == null)
             {
+                await _loggerService.Log(LogLevel.Warning, "GetAdresaById", $"Adresa sa id-em {adresaId} nije pronađena.");
                 return NoContent();
             }
+            await _loggerService.Log(LogLevel.Information, "GetAdresaById", $"Adresa sa id-em {adresaId} je uspešno pronađena.");
             return Ok(_mapper.Map<AdresaDto>(adresa));
         }
         [HttpPost]
@@ -60,11 +68,13 @@ namespace AdresaService.Controllers
                 await _adresaRepository.SaveChangesAsync();
 
                 string lokacija = _linkGenerator.GetPathByAction("GetAdresaById", "Adresa", new { adresaId = newAdresa.AdresaId });
+                await _loggerService.Log(LogLevel.Information, "CreateAdresa", $"Adresa sa vrednostima: {JsonConvert.SerializeObject(adresa)} je uspešno kreirana.");
 
                 return Created(lokacija, _mapper.Map<AdresaConformationDto>(newAdresa));
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                await _loggerService.Log(LogLevel.Error, "CreateAdresa", $"Greška prilikom unosa adrese sa vrednostima: {JsonConvert.SerializeObject(adresa)}.", e);
                 return StatusCode(StatusCodes.Status500InternalServerError, "Create Error");
             }
         }
@@ -77,16 +87,19 @@ namespace AdresaService.Controllers
 
                 if (adresa == null)
                 {
+                    await _loggerService.Log(LogLevel.Warning, "DeleteAdresa", $"Adresa sa id-em {adresaId} nije pronađena.");
                     return NotFound();
                 }
 
                 await _adresaRepository.DeleteAdresa(adresaId);
                 await _adresaRepository.SaveChangesAsync();
+                await _loggerService.Log(LogLevel.Information, "DeleteAdresa", $"Adresa sa id-em {adresaId} je uspešno obrisana. Obrisane vrednosti: {JsonConvert.SerializeObject(adresa)}");
 
                 return Ok();
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                await _loggerService.Log(LogLevel.Error, "DeleteAdresa", $"Greška prilikom brisanja adrese sa id-em {adresaId}.", e);
                 return StatusCode(StatusCodes.Status500InternalServerError, "Delete error");
             }
         }
@@ -97,9 +110,10 @@ namespace AdresaService.Controllers
             try
             {
                 var oldAdresa = await _adresaRepository.GetAdresaById(adresaUpdate.AdresaId);
-
-                if(oldAdresa == null)
+                var stareVrednosti = JsonConvert.SerializeObject(oldAdresa);
+                if (oldAdresa == null)
                 {
+                    await _loggerService.Log(LogLevel.Warning, "UpdateAdresa", $"Adresa sa id-em {adresaUpdate.AdresaId} nije pronađena.");
                     return NotFound();
                 }
 
@@ -108,11 +122,13 @@ namespace AdresaService.Controllers
                 _mapper.Map(adresa, oldAdresa);
 
                 await _adresaRepository.SaveChangesAsync();
+                await _loggerService.Log(LogLevel.Information, "UpdateAdresa", $"Adresa sa id-em {adresaUpdate} je uspešno izmenjena. Stare vrednosti su: {stareVrednosti}");
 
                 return Ok(_mapper.Map<AdresaDto>(adresa));
             }
-            catch (Exception)
+            catch (Exception e)
             {
+                await _loggerService.Log(LogLevel.Error, "UpdateAdresa", $"Greška prilikom izmene adrese sa id-em {adresaUpdate}.", e);
                 return StatusCode(StatusCodes.Status500InternalServerError, "Update error");
             }
         }
