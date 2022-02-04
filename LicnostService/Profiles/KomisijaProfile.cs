a2f968e8f2f9ea602770de76a3631f4cf92db2a3
Stefan Bulaja <stefan.bulaja99@uns.ac.rs>
2022-02-04 23:48:45 +0100

    Add Licnost and Komisija CRUD

---
+++ LicnostService/Profiles/KomisijaProfile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using LicnostService.Entities;
+using LicnostService.Entities.Confirmations;
+using LicnostService.Models.Komisija;
+
+namespace LicnostService.Profiles
+{
+    public class KomisijaProfile : Profile
+    {
+
+        public KomisijaProfile()
+        {
+            CreateMap<Komisija, KomisijaDTO>();
+            CreateMap<KomisijaCreateDTO, Komisija>()
+                .ForMember(
+                    dest => dest.ClanoviKomisije,
+                    opt => opt.Ignore());
+            CreateMap<Guid, Licnost>()
+                .ForMember(
+                    dest => dest.LicnostId,
+                    opt => opt.MapFrom(src => src));
+            CreateMap<KomisijaUpdateDTO, Komisija>()
+                .ForMember(
+                    dest => dest.ClanoviKomisije,
+                    opt => opt.Ignore());
+            CreateMap<Komisija, Komisija>();
+            CreateMap<KomisijaConfirmation, KomisijaConfirmationDTO>();
+            CreateMap<Komisija, KomisijaConfirmationDTO>();
+            CreateMap<Komisija, KomisijaConfirmation>();
+        }
+
+
+    }
+}
+
+
+
+        
---
+++ LicnostService/Profiles/LicnostProfile.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using LicnostService.Entities;
+using LicnostService.Models.Licnost;
+
+namespace LicnostService.Profiles
+{
+    public class LicnostProfile : Profile
+    {
+        public LicnostProfile()
+        {
+            CreateMap<Licnost, LicnostCreateDTO>().ReverseMap();
+            CreateMap<LicnostUpdateDTO, Licnost>();
+            CreateMap<Licnost, Licnost>();
+            CreateMap<Licnost, LicnostDTO>().ReverseMap();
+        }
+    }
+}
